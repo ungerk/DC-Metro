d9d8fff6ef1f86b42d788e60c98cd952d8b80fcd
Katherine Unger <katherine.unger@gmail.com>
2015-01-06 21:04:11 -0500

    lists all metro stops

---
+++ metro.py
@@ -0,0 +1,16 @@
+import sys,os
+import subprocess
+import urllib2
+import time
+import cPickle
+
+stations = []
+for i in range(0,116):
+    url_str = "http://www.wmata.com/rider_tools/pids/showpid.cfm?station_id=" + str(i)
+    url = urllib2.urlopen(url_str)
+    lines = url.readlines()
+    station_name = lines[5].strip()[7:-8]
+    if station_name != '':
+        stations.append([i,station_name,url_str])
+        print stations[-1]
+    time.sleep(0.1)
